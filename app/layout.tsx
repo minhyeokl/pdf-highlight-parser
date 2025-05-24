f30029d64fac2fcc2b2b2ac249171bf44b081073
minhyeokl <whereverigotheygo@gmail.com>
2025-05-24 19:03:26 +0900

    잡상인은 나가쇼

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
         <Analytics/>
       </body>
